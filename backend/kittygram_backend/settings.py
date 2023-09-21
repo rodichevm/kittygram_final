32d51782ebf8e62afc5d11997625d0625f9bc7ab
Michael Rodichev <rodichevmichael@gmail.com>
2023-09-21 15:21:38 +0300

    fixed settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG_VALUE')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1 kittygrimes.ddns.net').split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
